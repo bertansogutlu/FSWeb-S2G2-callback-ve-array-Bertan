b22d81c0a866040eb622076feeb9c0f7b142f0ba
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-01-04 00:39:58 +0300

    Guncellendi

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function Yillar(arr, callback) {
 
 function Kazananlar(arr, callback) {
 	const finals = callback(arr);
-	const kazananlar = finals.map( obj => (obj["Home Team Goals"] >= obj["Away Team Goals"] ? obj["Home Team Name"] : obj["Away Team Name"])  )
+	const kazananlar = finals.map( obj => (obj["Home Team Goals"] > obj["Away Team Goals"] ? obj["Home Team Name"] : obj["Away Team Name"])  )
 	return kazananlar;
 }
 
@@ -78,7 +78,7 @@ function YillaraGoreKazananlar(arr, callbackFinaller, callbackYillar, callbackKa
 	const yillar = callbackYillar(arr, callbackFinaller);
 	const kazananlar = callbackKazananlar(arr, callbackFinaller);
 	const tabela = [];
-	for (let i in finaller) tabela.push(`${yillar[i]} yılında, ${kazananlar[i]} dünya kupasını kazandı!`);
+	for (let i in finaller) {tabela.push(`${yillar[i]} yılında, ${kazananlar[i]} dünya kupasını kazandı!`)};
 	return tabela;
 }
 
